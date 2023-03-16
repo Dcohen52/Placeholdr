09f66efc1d1fa7231674771137410a43a660ea44
Dekel Cohen <dcohen52@gmail.com>
2023-03-16 10:59:44 +0200

    Add files via upload

---
+++ Examples/Example-2/Example-2.py
@@ -0,0 +1,17 @@
+from tempt import tempt
+
+# Create an instance of the tempt class with the path to the template file
+template = tempt("G:\\My Drive\\Development\\Python\\TemplateSystem\\templates\\template.html")
+
+# Define a dictionary of values to substitute in the template
+context = {
+  "title": "Example Page",
+  "heading": "This is an example page",
+  "content": "This is some example content"
+}
+
+# Render the template with the context dictionary
+output = template.render(context)
+
+# Print the output
+print(output)
